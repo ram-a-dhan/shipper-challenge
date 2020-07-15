2a315ab941ddf6034d1cbaa203fb23b53d7dc3ff
ram-a-dhan <ram.adhan@outlook.com>
2020-07-15 22:04:28 +0700

    dynamic sidebar done;

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import Content from './Content';
 
 export default function Body() {
+  const [ active, setActive ] = useState({
+    beranda: false,
+    pickup: false,
+    order: false,
+    driver: true,
+    merchant: false,
+    laporan: false,
+    profil: false,
+  });
   return (
     <div className="Body container d-flex flex-row flex-nowrap justify-content-center align-items-start">
       <div className="d-flex flex-wrap w-100">
-        <Sidebar />
-        <Content />
+        <Sidebar active={active} setActive={setActive} />
+        <Content active={active} setActive={setActive} />
       </div>
 
     </div>
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -11,51 +11,83 @@ import {
 } from 'react-icons/fa';
 import './Sidebar.css';
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+
+  const handleActive = (target) => {
+    let activeCopy = { ...props.active };
+    // let nextActive = !activeCopy[target];
+    for (let key in activeCopy) {
+      activeCopy[key] = false;
+    }
+    activeCopy[target] = true;
+    props.setActive(activeCopy);
+  };
+
 	return (
 		<div className="col-12 col-sm-12 col-md-3 col-xl-2 p-0">
 			<ul className="SidebarList">
-				<IconContext.Provider value={{ size: '2.5vh', color: '#405060' }}>
+				<IconContext.Provider value={{ size: '2.5vh' }}>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + "text-dark"}>
+          <a href="#"
+            className={"" + (props.active.beranda ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('beranda')}
+          >
 						<li className="py-3 pl-3">
-							<FaHome /> Beranda
+							<FaHome color={props.active.beranda ? "#FF003D" : "#405060" } /> Beranda
 						</li>
 					</a>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + "text-dark"}>
+          <a href="#"
+            className={"" + (props.active.pickup ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('pickup')}
+          >
 						<li className="py-3 pl-3">
-							<FaCalendarDay /> Pickup
+							<FaCalendarDay color={props.active.pickup ? "#FF003D" : "#405060" } /> Pickup
 						</li>
 					</a>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + "text-dark"}>
+          <a href="#"
+            className={"" + (props.active.order ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('order')}
+          >
 						<li className="py-3 pl-3">
-							<FaDollyFlatbed /> Order
+							<FaDollyFlatbed color={props.active.order ? "#FF003D" : "#405060" } /> Order
 						</li>
 					</a>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + " text-danger" + " SidebarActive"}>
+          <a href="#"
+            className={"" + (props.active.driver ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('driver')}
+          >
 						<li className="py-3 pl-3">
-							<FaIdCard color="#FF003D" /> Driver
+							<FaIdCard color={props.active.driver ? "#FF003D" : "#405060" } /> Driver
 						</li>
 					</a>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + "text-dark"}>
+          <a href="#"
+            className={"" + (props.active.merchant ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('merchant')}
+          >
 						<li className="py-3 pl-3">
-							<FaStore /> Merchant
+							<FaStore color={props.active.merchant ? "#FF003D" : "#405060" } /> Merchant
 						</li>
 					</a>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + "text-dark"}>
+          <a href="#"
+            className={"" + (props.active.laporan ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('laporan')}
+          >
 						<li className="py-3 pl-3">
-							<FaClipboardCheck /> Laporan
+							<FaClipboardCheck  color={props.active.laporan ? "#FF003D" : "#405060" } /> Laporan
 						</li>
 					</a>
 					{/* eslint-disable-next-line */}
-					<a href="#" className={"" + "text-dark"}>
+          <a href="#"
+            className={"" + (props.active.profil ? " text-danger SidebarActive" : " text-dark")}
+            onClick={() => handleActive('profil')}
+          >
 						<li className="py-3 pl-3">
-							<FaUserCircle /> Profil
+							<FaUserCircle  color={props.active.profil ? "#FF003D" : "#405060" } /> Profil
 						</li>
 					</a>
 				</IconContext.Provider>
